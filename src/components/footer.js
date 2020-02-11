87d85947557081dd8bcc154fab95591513dd9d7f
ringuralte <ringuralte@gmail.com>
2020-02-11 11:32:32 +0530

    box shadow, fixed header and margins

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,27 +1,51 @@
-import React from 'react' 
-import { Link } from 'gatsby'
+import React from "react"
+import { Link } from "gatsby"
 
-import footerStyles from './footer.module.scss'
+import footerStyles from "./footer.module.scss"
 
 const Footer = () => {
   return (
     <footer className={footerStyles.footer}>
       <div className={footerStyles.wrapper}>
         <div className={footerStyles.block}>
-          <p>Sunhlukawn, West Serzawl Road,</p>
-          <p>Lengpui,</p> 
-          <p>Mizoram - 796410</p>
+          <span>Sunhlukawn, Serzawl Road</span>
+          <span>Lengpui,</span>
+          <span>Mizoram - 796410</span>
         </div>
         <div className={footerStyles.block}>
           <p>email: info@gooseberrymedia.in</p>
-          <p><a className={footerStyles.phone} href="tel: +91996114838">Tel : +91996114838</a><a className={footerStyles.phone} href="tel: +919650657715"> +919650657715</a></p>
+          <p>
+            <a className={footerStyles.phone} href="tel: +91996114838">
+              Tel : +91996114838
+            </a>
+            <a className={footerStyles.phone} href="tel: +919650657715">
+              {" "}
+              +919650657715
+            </a>
+          </p>
         </div>
         <div className={footerStyles.block}>
           <ul className={footerStyles.nav}>
-            <li><Link className={footerStyles.navItem} to='/'>Home</Link></li>
-            <li><Link className={footerStyles.navItem} to='/gallery'>Gallery</Link></li>
-            <li><Link className={footerStyles.navItem} to='/about'>About</Link></li>
-            <li><Link className={footerStyles.navItem} to='/contact'>Contact</Link></li>
+            <li>
+              <Link className={footerStyles.navItem} to="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className={footerStyles.navItem} to="/gallery">
+                Gallery
+              </Link>
+            </li>
+            <li>
+              <Link className={footerStyles.navItem} to="/about">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link className={footerStyles.navItem} to="/contact">
+                Contact
+              </Link>
+            </li>
           </ul>
         </div>
       </div>
@@ -29,7 +53,6 @@ const Footer = () => {
         <p>&copy; gooseberrymedia</p>
       </div>
     </footer>
-
   )
 }
 
